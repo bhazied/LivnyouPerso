a39d2ba801456ded498733cb0ae82083f3b6e796
Zied Ben Hadj Amor <zied.benhadjamor@continuousnet.com>
2017-05-03 17:51:11 +0100

    fixe translate country

--- src/ContinuousNet/LivnYouBundle/Controller/TranslationCountryRESTController.php
+++ src/ContinuousNet/LivnYouBundle/Controller/TranslationCountryRESTController.php
@@ -53,10 +53,10 @@ class TranslationCountryRESTController extends BaseRESTController
      * @return Response
      *
      */
-    public function getAction($id)
+    public function getAction($idEntity)
     {
         try {
-            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:TranslationCountry')->get(['id' => $id]);
+            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:TranslationCountry')->get(['id' => $idEntity]);
             $this->createSubDirectory($entity);
             return $entity;
         } catch (\Exception $e) {
@@ -85,10 +85,10 @@ class TranslationCountryRESTController extends BaseRESTController
             $this->createSubDirectory(new TranslationCountry());
             $offset = $paramFetcher->get('offset');
             $limit = $paramFetcher->get('limit');
-            $filter_operators = $paramFetcher->get('filter_operators') ? $paramFetcher->get('filter_operators') : array();
-            $order_by = $paramFetcher->get('order_by') ? $paramFetcher->get('order_by') : array();
+            $filterOperators = $paramFetcher->get('filter_operators') ? $paramFetcher->get('filter_operators') : array();
+            $orderBy = $paramFetcher->get('order_by') ? $paramFetcher->get('order_by') : array();
             $filters = !is_null($paramFetcher->get('filters')) ? $paramFetcher->get('filters') : array();
-            $params = compact('offset', 'limit', 'filter_operators', 'order_by', 'filters');
+            $params = compact('offset', 'limit', 'filterOperators', 'orderBy', 'filters');
             $data = array(
                 'inlineCount' => 0,
                 'results' => array()
@@ -132,14 +132,14 @@ class TranslationCountryRESTController extends BaseRESTController
      * @View(serializerEnableMaxDepthChecks=true)
      *
      * @param Request $request
-     * @param $id
+     * @param $idEntity
      *
      * @return Response
      */
-    public function putAction(Request $request, $id)
+    public function putAction(Request $request, $idEntity)
     {
         try {
-            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:TranslationCountry')->get(['id' => $id]);
+            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:TranslationCountry')->get(['id' => $idEntity]);
             $request->setMethod('PATCH'); //Treat all PUTs as PATCH
             $form = $this->createForm(TranslationCountryType::class, $entity, array('method' => $request->getMethod()));
             $this->removeExtraFields($request, $form);
@@ -160,13 +160,13 @@ class TranslationCountryRESTController extends BaseRESTController
      * @View(serializerEnableMaxDepthChecks=true)
      *
      * @param Request $request
-     * @param $id
+     * @param $idEntity
      *
      * @return Response
      */
-    public function patchAction(Request $request, $id)
+    public function patchAction(Request $request, $idEntity)
     {
-        return $this->putAction($request, $id);
+        return $this->putAction($request, $idEntity);
     }
 
     /**
@@ -175,14 +175,14 @@ class TranslationCountryRESTController extends BaseRESTController
      * @View(statusCode=204)
      *
      * @param Request $request
-     * @param $id
+     * @param $idEntity
      *
      * @return Response
      */
-    public function deleteAction(Request $request, $id)
+    public function deleteAction($idEntity)
     {
         try {
-            $this->getDoctrine()->getRepository('LivnYouBundle:TranslationCountry')->delete($id);
+            $this->getDoctrine()->getRepository('LivnYouBundle:TranslationCountry')->delete($idEntity);
             return null;
         } catch (\Exception $e) {
             return FOSView::create($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
--- src/ContinuousNet/LivnYouBundle/Repository/TranslationCountryRepository.php
+++ src/ContinuousNet/LivnYouBundle/Repository/TranslationCountryRepository.php
@@ -13,16 +13,17 @@ class TranslationCountryRepository extends EntityRepository implements IReposito
 {
     public function getAll($params = [])
     {
-        $qb = $this->createQueryBuilder('translationCountry');
-        $qb->leftJoin('ContinuousNet\LivnYouBundle\Entity\Country', 'country', \Doctrine\ORM\Query\Expr\Join::WITH, 'translationCountry.country = country.id');
-        $qb->leftJoin('ContinuousNet\LivnYouBundle\Entity\User', 'creator_user', \Doctrine\ORM\Query\Expr\Join::WITH, 'translationCountry.creatorUser = creator_user.id');
-        $qb->leftJoin('ContinuousNet\LivnYouBundle\Entity\User', 'modifier_user', \Doctrine\ORM\Query\Expr\Join::WITH, 'translationCountry.modifierUser = modifier_user.id');
+        $qBuilder = $this->createQueryBuilder('translationCountry');
+        $qBuilder->leftJoin('ContinuousNet\LivnYouBundle\Entity\Country', 'country', \Doctrine\ORM\Query\Expr\Join::WITH, 'translationCountry.country = country.id');
+        $qBuilder->leftJoin('ContinuousNet\LivnYouBundle\Entity\User', 'creator_user', \Doctrine\ORM\Query\Expr\Join::WITH, 'translationCountry.creatorUser = creator_user.id');
+        $qBuilder->leftJoin('ContinuousNet\LivnYouBundle\Entity\User', 'modifier_user', \Doctrine\ORM\Query\Expr\Join::WITH, 'translationCountry.modifierUser = modifier_user.id');
         $textFields = array('translationCountry.locale', 'translationCountry.name');
         $memberOfConditions = array();
         foreach ($params['filters'] as $field => $value) {
             if (substr_count($field, '.') > 1) {
                 if ($value == 'true' || $value == 'or' || $value == 'and') {
                     list($entityName, $listName, $listItem) = explode('.', $field);
+                    $entityName = null;
                     if (!isset($memberOfConditions[$listName])) {
                         $memberOfConditions[$listName] = array('items' => array(), 'operator' => 'or');
                     }
@@ -37,39 +38,39 @@ class TranslationCountryRepository extends EntityRepository implements IReposito
             $key = str_replace('.', '', $field);
             if (!empty($value)) {
                 if (in_array($field, $textFields)) {
-                    if (isset($filter_operators[$field]) && $filter_operators[$field] == 'eq') {
-                        $qb->andWhere($qb->expr()->eq($field, $qb->expr()->literal($value)));
+                    if (isset($params['filterOperators'][$field]) && $params['filterOperators'][$field] == 'eq') {
+                        $qBuilder->andWhere($qBuilder->expr()->eq($field, $qBuilder->expr()->literal($value)));
                     } else {
-                        $qb->andWhere($qb->expr()->like($field, $qb->expr()->literal('%' . $value . '%')));
+                        $qBuilder->andWhere($qBuilder->expr()->like($field, $qBuilder->expr()->literal('%' . $value . '%')));
                     }
                 } else {
-                    $qb->andWhere($field.' = :'.$key.'')->setParameter($key, $value);
+                    $qBuilder->andWhere($field.' = :'.$key.'')->setParameter($key, $value);
                 }
             }
         }
         foreach ($memberOfConditions as $listName => $memberOfCondition) {
             if (!empty($memberOfCondition['items'])) {
                 if ($memberOfCondition['operator'] == 'or') {
-                    $orX = $qb->expr()->orX();
+                    $orX = $qBuilder->expr()->orX();
                     foreach ($memberOfCondition['items'] as $i => $item) {
-                        $orX->add($qb->expr()->isMemberOf(':'.$listName.'_value_'.$i, 'translationCountry.'.$listName));
-                        $qb->setParameter($listName.'_value_'.$i, $item);
+                        $orX->add($qBuilder->expr()->isMemberOf(':'.$listName.'_value_'.$i, 'translationCountry.'.$listName));
+                        $qBuilder->setParameter($listName.'_value_'.$i, $item);
                     }
-                    $qb->andWhere($orX);
+                    $qBuilder->andWhere($orX);
                 } elseif ($memberOfCondition['operator'] == 'and') {
-                    $andX = $qb->expr()->andX();
+                    $andX = $qBuilder->expr()->andX();
                     foreach ($memberOfCondition['items'] as $i => $item) {
-                        $andX->add($qb->expr()->isMemberOf(':'.$listName.'_value_'.$i, 'translationCountry.'.$listName));
-                        $qb->setParameter($listName.'_value_'.$i, $item);
+                        $andX->add($qBuilder->expr()->isMemberOf(':'.$listName.'_value_'.$i, 'translationCountry.'.$listName));
+                        $qBuilder->setParameter($listName.'_value_'.$i, $item);
                     }
-                    $qb->andWhere($andX);
+                    $qBuilder->andWhere($andX);
                 }
             }
         }
-        $qbList = clone $qb;
-        $qb->select('count(translationCountry.id)');
-        $data['inlineCount'] = $qb->getQuery()->getSingleScalarResult();
-        foreach ($params['order_by'] as $field => $direction) {
+        $qbList = clone $qBuilder;
+        $qBuilder->select('count(translationCountry.id)');
+        $data['inlineCount'] = $qBuilder->getQuery()->getSingleScalarResult();
+        foreach ($params['orderBy'] as $field => $direction) {
             $qbList->addOrderBy($field, $direction);
         }
         $qbList->select('translationCountry');
@@ -107,9 +108,9 @@ class TranslationCountryRepository extends EntityRepository implements IReposito
         return $entity;
     }
 
-    public function delete($id)
+    public function delete($idEntity)
     {
-        $entity = $this->find($id);
+        $entity = $this->find($idEntity);
         $this->getEntityManager()->remove($entity);
         $this->getEntityManager()->flush();
     }
