98f26bc21b92bb933d1254cd49bc621704463756
Zied Ben Hadj Amor <zied.benhadjamor@continuousnet.com>
2017-05-03 17:39:07 +0100

    fixe tempalte

--- src/ContinuousNet/LivnYouBundle/Controller/TemplateRESTController.php
+++ src/ContinuousNet/LivnYouBundle/Controller/TemplateRESTController.php
@@ -53,10 +53,10 @@ class TemplateRESTController extends BaseRESTController
      * @return Response
      *
      */
-    public function getAction($id)
+    public function getAction($idEntity)
     {
         try {
-            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:Template')->get(['id' => $id]);
+            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:Template')->get(['id' => $idEntity]);
             $this->createSubDirectory($entity);
             return $entity;
         } catch (\Exception $e) {
@@ -85,10 +85,18 @@ class TemplateRESTController extends BaseRESTController
             $this->createSubDirectory(new Template());
             $offset = $paramFetcher->get('offset');
             $limit = $paramFetcher->get('limit');
-            $filter_operators = $paramFetcher->get('filter_operators') ? $paramFetcher->get('filter_operators') : array();
-            $order_by = $paramFetcher->get('order_by') ? $paramFetcher->get('order_by') : array();
+            $filterOperators = $paramFetcher->get('filter_operators') ? $paramFetcher->get('filter_operators') : array();
+            $orderBy = $paramFetcher->get('order_by') ? $paramFetcher->get('order_by') : array();
             $filters = !is_null($paramFetcher->get('filters')) ? $paramFetcher->get('filters') : array();
-            $params = compact('offset', 'limit', 'filter_operators', 'order_by', 'filters');
+            $roles = $this->getUser()->getRoles();
+            if (!empty($roles)) {
+                foreach ($roles as $role) {
+                    if (substr_count($role, 'MAN') > 0) {
+                        $filters['template.creatorUser'] = $filters['template.modifierUser'] = $this->getUser()->getId();
+                    }
+                }
+            }
+            $params = compact('offset', 'limit', 'filterOperators', 'orderBy', 'filters');
             $data = array(
                 'inlineCount' => 0,
                 'results' => array()
@@ -149,15 +157,14 @@ class TemplateRESTController extends BaseRESTController
      * @View(serializerEnableMaxDepthChecks=true)
      *
      * @param Request $request
-     * @param $id
+     * @param $idEntity
      *
      * @return Response
      */
-    public function putAction(Request $request, $id)
+    public function putAction(Request $request, $idEntity)
     {
         try {
-            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:Template')->findOneById($id);
-            $em = $this->getDoctrine()->getManager();
+            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:Template')->findOneById($idEntity);
             $request->setMethod('PATCH'); //Treat all PUTs as PATCH
             $roles = $this->getUser()->getRoles();
             if (!empty($roles)) {
@@ -198,13 +205,13 @@ class TemplateRESTController extends BaseRESTController
      * @View(serializerEnableMaxDepthChecks=true)
      *
      * @param Request $request
-     * @param $id
+     * @param $idEntity
      *
      * @return Response
      */
-    public function patchAction(Request $request, $id)
+    public function patchAction(Request $request, $idEntity)
     {
-        return $this->putAction($request, $id);
+        return $this->putAction($request, $idEntity);
     }
 
     /**
@@ -213,14 +220,14 @@ class TemplateRESTController extends BaseRESTController
      * @View(statusCode=204)
      *
      * @param Request $request
-     * @param $id
+     * @param $idEntity
      *
      * @return Response
      */
-    public function deleteAction(Request $request, $id)
+    public function deleteAction($idEntity)
     {
         try {
-            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:Template')->findOneById($id);
+            $entity = $this->getDoctrine()->getRepository('LivnYouBundle:Template')->findOneById($idEntity);
             $roles = $this->getUser()->getRoles();
             if (!empty($roles)) {
                 foreach ($roles as $role) {
@@ -231,7 +238,7 @@ class TemplateRESTController extends BaseRESTController
                     }
                 }
             }
-            $this->getDoctrine()->getRepository('LivnYouBundle:Template')->delete($id);
+            $this->getDoctrine()->getRepository('LivnYouBundle:Template')->delete($idEntity);
             return null;
         } catch (\Exception $e) {
             return FOSView::create($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
